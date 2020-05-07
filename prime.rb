0eadfd95593c027097b9e0601f411693974adbfc
Anette Rivera <anettem.rivera@gmail.com>
2020-05-07 23:08:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,11 @@
-require 'prime'
-
-def prime?(numbers)
-  numbers.prime?
-end 
+def prime?(number)
+  first = 2
+  if number > 1
+    range = (first..number-1).to_a 
+    range.none? do |num_to_test| 
+      number % num_to_test == 0
+    end
+  else
+    false
+  end
+end
